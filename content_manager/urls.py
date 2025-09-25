50dffd36c2b35e1f1a7aff668f446159d1f2330e
gzuba <55127799+gzubicki@users.noreply.github.com>
2025-09-25 21:28:44 +0200

    Serve media files in development

--- content_manager/urls.py
+++ content_manager/urls.py
@@ -1,6 +1,9 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path
 from django.views.generic import TemplateView
+
 from apps.accounts.views import telegram_login, telegram_bind
 
 urlpatterns = [
@@ -10,3 +13,6 @@ urlpatterns = [
     re_path(r"^sw\.js$", TemplateView.as_view(template_name="sw.js", content_type="application/javascript")),
     path("manifest.webmanifest", TemplateView.as_view(template_name="manifest.webmanifest", content_type="application/manifest+json")),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
